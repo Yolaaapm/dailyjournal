d56878ffa3130bb8ac0a3360de240027ee61e8f8
Yolaaapm <monikfiola@gmail.com>
2025-01-08 09:27:08 +0700

    Ubah footer dan about me udinus index

--- index.php
+++ index.php
@@ -269,7 +269,7 @@ include "koneksi.php";
             <h3 class="lead">A11.2023.15413</h3>
             <h1 class="fw-bold">Fiola Putri Monika</h1>
             Program Studi Teknik Informatika<br />
-            <a href="https://dinus.ac.id/" class="fw-bold text-decoration-none"
+            <a href="https://dinus.ac.id/" class="fw-bold text-decoration-none text-dark"
               >Universitas Dian Nuswantoro</a
             >
           </div>
