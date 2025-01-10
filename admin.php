119a0c51f6e133b64bbf27b0865b771d1ec2dda6
Yolaaapm <monikfiola@gmail.com>
2025-01-10 19:12:31 +0700

    hapus perubahan profil

--- admin.php
+++ admin.php
@@ -78,7 +78,7 @@ if (!isset($_SESSION['username'])) {
                     <?= $_SESSION['username']?>
                 </a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="profil.php" href="admin.php?page=profil">Profil fio</a></li> 
+                    <li><a class="dropdown-item" href="profil.php">Profil fio</a></li> 
                     <li><a class="dropdown-item" href="logout.php">Logout</a></li> 
                 </ul>
             </li> 
